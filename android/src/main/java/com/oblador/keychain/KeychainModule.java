81eb0f6963d4fb92ffdf586386d26beef7f7f342
Sergei Gerasimov <sergei.gerasimov@moneyball.com.au>
2022-06-21 21:32:31 +0800

    semi

--- android/src/main/java/com/oblador/keychain/KeychainModule.java
+++ android/src/main/java/com/oblador/keychain/KeychainModule.java
@@ -590,8 +590,8 @@ public class KeychainModule extends ReactContextBaseJavaModule {
     return AccessControl.BIOMETRY_ANY.equals(accessControl)
       || AccessControl.BIOMETRY_CURRENT_SET.equals(accessControl)
       || AccessControl.BIOMETRY_ANY_OR_DEVICE_PASSCODE.equals(accessControl)
-      || AccessControl.BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE.equals(accessControl);
-      || AccessControl.USER_PRESENCE.equals(accessControl)
+      || AccessControl.BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE.equals(accessControl)
+      || AccessControl.USER_PRESENCE.equals(accessControl);
   }
 
   private void addCipherStorageToMap(@NonNull final CipherStorage cipherStorage) {
